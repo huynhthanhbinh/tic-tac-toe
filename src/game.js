8eab651404130614bcce356d4818b2218715fc6d
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-04-16 01:27:09 +0700

    finish time-travel reverse

--- src/game.js
+++ src/game.js
@@ -6,7 +6,9 @@ import GameModal from "./game-modal";
 import * as algorithm from "./algorithm";
 import "./game.css";
 
+let hasWinner = false;
 const boardSize = algorithm.boardSize;
+const winArray = algorithm.winArray;
 
 class Game extends Component {
   constructor(props) {
@@ -17,69 +19,100 @@ class Game extends Component {
     this.state = {
       stepNumber: 0,
       isHistorySortAsc: true,
-      historicWinArray: [false],
       historicValueArray: [emptyValueArray],
       historicColorArray: [emptyColorArray],
     };
   }
 
   goTo(step) {
-    this.setState({
-      stepNumber: step,
-    });
+    if (!hasWinner) {
+      this.setState({
+        stepNumber: step,
+      });
+    }
   }
 
   render = () => {
-    const isWinStep = this.state.historicWinArray[this.state.stepNumber];
-
+    const xIsNext = this.state.stepNumber % 2 === 0;
     return (
       <div className="game">
         <div className="game-main">
-          <GameStatus xIsNext={this.state.stepNumber % 2 === 0} />
+          <GameStatus xIsNext={!hasWinner ? xIsNext : !xIsNext} />
           <GameBoard
             onClickElement={(row, col) => this.onClickElement(row, col)}
             valueArray={this.state.historicValueArray[this.state.stepNumber]}
-            colorArray={this.state.historicColorArray[isWinStep ? 1 : 0]}
+            colorArray={this.state.historicColorArray[hasWinner ? 1 : 0]}
           />
           <GameInfo />
           <GameModal ref={this.gameModal} />
         </div>
         <div className="game-vr"></div>
         <div className="game-sub">
-          <ol>{this.renderMoveList()}</ol>
+          <button className="btnReverse" onClick={() => this.onClickRevert()}>
+            Reverse order
+          </button>
+          <ol className="stepList">{this.renderStepList()}</ol>
         </div>
       </div>
     );
   };
 
-  onClickElement = (row, col) => {
-    const xIsNext = this.state.stepNumber % 2 === 0;
-    const historicValueArray = this.state.historicValueArray.slice(0, this.state.stepNumber + 1);
-    const nextValueArray = historicValueArray[historicValueArray.length - 1].map((arr) => arr.slice());
-
-    nextValueArray[row][col] = xIsNext ? "X" : "O";
-
-    const isWin = false;
-
+  onClickRevert = () => {
     this.setState({
-      historicWinArray: this.state.historicWinArray.concat(isWin),
-      historicValueArray: historicValueArray.concat([nextValueArray]),
-      stepNumber: historicValueArray.length,
+      isHistorySortAsc: !this.state.isHistorySortAsc,
     });
   };
 
-  renderMoveList = () => {
+  onClickElement = (row, col) => {
+    if (!hasWinner) {
+      const xIsNext = this.state.stepNumber % 2 === 0;
+      const historicValueArray = this.state.historicValueArray.slice(
+        0,
+        this.state.stepNumber + 1
+      );
+      const nextValueArray = historicValueArray[
+        historicValueArray.length - 1
+      ].map((arr) => arr.slice());
+      const nextColorArray = this.state.historicColorArray[0].map((arr) =>
+        arr.slice()
+      );
+
+      nextValueArray[row][col] = xIsNext ? "X" : "O";
+
+      if (algorithm.isWin(nextValueArray, row, col)) {
+        hasWinner = true;
+        for (let i = 0; i < 5; i++) {
+          nextColorArray[winArray[i].row][winArray[i].col] = "darkkhaki";
+        }
+        this.gameModal.current.show();
+      }
+
+      this.setState({
+        historicValueArray: historicValueArray.concat([nextValueArray]),
+        historicColorArray: hasWinner
+          ? this.state.historicColorArray.concat([nextColorArray])
+          : this.state.historicColorArray,
+        stepNumber: historicValueArray.length,
+      });
+    }
+  };
+
+  renderStepList = () => {
     const stepNumber = this.state.stepNumber;
-    return this.state.historicValueArray.map((valueArray, step) => {
+    const moveList = this.state.historicValueArray.map((valueArray, step) => {
       const buttonLabel = step ? "Go to step #" + step : "Go to game start";
       return (
         <li id={step} key={step}>
-          <button className={step === stepNumber ? "highlight" : "non-highlight"} onClick={() => this.goTo(step)}>
+          <button
+            className={step === stepNumber ? "highlight" : "non-highlight"}
+            onClick={() => this.goTo(step)}
+          >
             {buttonLabel}
           </button>
         </li>
       );
     });
+    return this.state.isHistorySortAsc ? moveList : moveList.reverse();
   };
 }
 
